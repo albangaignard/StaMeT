31fab34160f8185fc812e31180c3b095bd1712d1
Fadoua Ben Azzouz <fadoua.benazzouz@ico.unicancer.fr>
2017-11-14 09:28:59 +0100

    Add new file

---
+++ biostats modules/run_exp.R
@@ -0,0 +1,18 @@
+
+
+CD C:\Users\f-benazzouz\Desktop\biostats modules
+
+"C:\Users\f-benazzouz\Documents\R\R-3.4.2\bin\R.exe" R --vanilla< create_directories.R
+
+
+"C:\Users\f-benazzouz\Documents\R\R-3.4.2\bin\R.exe" R --vanilla< directories.R
+
+
+"C:\Users\f-benazzouz\Documents\R\R-3.4.2\bin\R.exe" R --vanilla --args 10000 135 15 0.9 0.5 "FC.txt" < counts_data_simulator.R
+
+
+"C:\Users\f-benazzouz\Documents\R\R-3.4.2\bin\R.exe"  R --vanilla --args 10000 135 15 0.9 0.5  1.4 0.8 < Array_simulator.R
+
+
+
+"C:\Users\f-benazzouz\Documents\R\R-3.4.2\bin\R.exe" R --vanilla --args "simulation" "DESeq" "Zscor" 135 15 135 15 < Cross_plateformes.R
