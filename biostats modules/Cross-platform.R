dbf74ee351d2c3a1dabbdc97d24bde3557fa5747
Fadoua Ben Azzouz <fadoua.benazzouz@ico.unicancer.fr>
2017-11-14 09:13:53 +0100

    Update Cross-platform.R

--- biostats modules/Cross-platform.R
+++ biostats modules/Cross-platform.R
@@ -1,16 +1,22 @@
 
+suppressMessages(source("package_loader.R"))
+load_it( c("MASS", "DESeq","DESeq2" ,"edgeR","limma","preprocessCore","clusterSim"))
+source("Repertoires.R")
+source("normalize_data.R")
+
 
 args <- commandArgs(trailingOnly=TRUE)
-dossier_data = args[1]
-Dossier_Normalisation = args[2]
-Dossier_output = args[3]
-prefix= args[4]
 
+prefix=args[1]
+Norm=args[2]
+St=args[3]
+Mn1=as.numeric(args[4])
+Mn2=as.numeric(args[5])
+Rn1=as.numeric(args[6])
+Rn2=as.numeric(args[7])
 ############################ Cross plateformes
 
-suppressMessages(source("package_loader.R"));
-load_it( c("MASS", "DESeq","DESeq2" ,"edgeR","limma","preprocessCore","clusterSim"));
-source("normalize_data.R")
+
 
  
 Standarisation=function(Ref,dat,St=c("Zscor","med_centering","Robust_Zscor","QN")){
@@ -46,7 +52,7 @@ if(St=="Zscor"){
 }
 
 
-Cross_platformes_normalisation=function(Microarray,RNA,Norm,St,Mn1,Mn2,Rn1,Rn2,common_sample=c("FALSE","TRUE")){
+Cross_platformes_normalisation=function(Microarray,RNA,Norm,St,Mn1,Mn2,Rn1,Rn2){
 
 	##  vérifier et supprimer les valeurs manquantes NA
 	 Microarray<-Microarray[apply(Microarray, 1, function(x) !any(is.na(x))),]
@@ -65,18 +71,7 @@ Cross_platformes_normalisation=function(Microarray,RNA,Norm,St,Mn1,Mn2,Rn1,Rn2,c
 	counts=RNA[genes_commun,]
 	microarrays=Microarray[genes_commun,]
 
-	## Cas ou les données sont appariées
-	if(common_sample=="TRUE"){
-	Cond1_common_samples<-intersect(colnames(RNA[,1:Rn1]),colnames(Microarray[,1:Mn1])) 
-	Cond2_common_samples<-intersect(colnames(RNA[,(Rn1+1):ncol(RNA)]),colnames(Microarray[,(Mn1+1):ncol(Microarray)]))
-	common_samples=c(Cond1_common_samples,Cond2_common_samples)
-	counts=RNA[genes_commun,common_samples]
-	Microarray=Microarray[genes_commun,common_samples]
-	Mn1<-Rn1<-length(Cond1_common_samples)
-	Mn2<-Rn2<-length(Cond2_common_samples)
-	} 
-	
-	
+
 		## Choix de la normalisation des données RNA seq
 		R_Norm<-Normalization(counts,Rn1,Rn2,Norm)
 		Design=R_Norm[[1]]
@@ -89,7 +84,7 @@ Cross_platformes_normalisation=function(Microarray,RNA,Norm,St,Mn1,Mn2,Rn1,Rn2,c
 		M=Standarisation(microarrays,microarrays,St)
 		
 		# Fusion
-		Fusion<-data.frame(M[,1:Mn1],R[,1:Rn1],M[,(Mn1+1):ncol(M)],R[,(Rn1+1):ncol(R)],stringsAsFactors=FALSE) #		   
+		Fusion<-cbind(M[,1:Mn1],R[,1:Rn1],M[,(Mn1+1):ncol(M)],R[,(Rn1+1):ncol(R)]) #		   
 		colnames(Fusion)=c(paste("Cond1",1:(Mn1+Rn1)),paste("Cond2",1:(Mn2+Rn2)))
 		DESIGN=factor(c(rep(1,(Mn1+Rn1)),rep(2,(Mn2+Rn2))))
 			   
@@ -97,8 +92,9 @@ Cross_platformes_normalisation=function(Microarray,RNA,Norm,St,Mn1,Mn2,Rn1,Rn2,c
            
 }       
  
- RNA<-read.table(file=paste0(dossier_data,"RNA_count_",prefix,"_pDE=",1-pi0,".txt"),sep="\t")
-Microarray<-read.table(file=paste0(dossier_data,"Array_",prefix,"_pDE=",1-pi0,".txt"),sep="\t")
+RNA<-read.table(file=file.path(data_simulation,paste0("RNA_count_",prefix,".txt")),sep="\t")
+Microarray<-read.table(file=file.path(data_simulation,paste0("Array_",prefix,".txt")),sep="\t")
 
-Cross_platformes<-Cross_platformes_normalisation(Microarray,RNA,Norm,St,n1,n2,n1,n2,common_sample)
-write.table( Cross_platformes$Fusion,file=paste0(Dossier_output,paste0("Fusion_",prefix,"_",Norm,"_",St,".txt")),sep="\t",row.names=T, col.names=T) 
+Cross_platformes<-Cross_platformes_normalisation(Microarray,RNA,Norm,St,Mn1,Mn2,Rn1,Rn2)
+
+write.table( Cross_platformes$Fusion,file=file.path(output,paste0("Fusion_",prefix,"_",Norm,"_",St,".txt")),sep="\t",row.names=T, col.names=T) 
