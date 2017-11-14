ebcec3ca67be821f63c322f35e0f4983a6ab05aa
Fadoua Ben Azzouz <fadoua.benazzouz@ico.unicancer.fr>
2017-11-14 09:30:44 +0100

    Update run_exp.R

--- biostats modules/run_exp.R
+++ biostats modules/run_exp.R
@@ -1,11 +1,10 @@
 
-
 CD C:\Users\f-benazzouz\Desktop\biostats modules
 
-"C:\Users\f-benazzouz\Documents\R\R-3.4.2\bin\R.exe" R --vanilla< create_directories.R
+"C:\Users\f-benazzouz\Documents\R\R-3.4.2\bin\R.exe" R --vanilla< Create_directories.R
 
 
-"C:\Users\f-benazzouz\Documents\R\R-3.4.2\bin\R.exe" R --vanilla< directories.R
+"C:\Users\f-benazzouz\Documents\R\R-3.4.2\bin\R.exe" R --vanilla< Directories.R
 
 
 "C:\Users\f-benazzouz\Documents\R\R-3.4.2\bin\R.exe" R --vanilla --args 10000 135 15 0.9 0.5 "FC.txt" < counts_data_simulator.R
