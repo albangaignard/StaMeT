dee2960fa961df4b169c4270ea09f731b47caa2b
Alban Gaignard <aban.gaignard@univ-nantes.fr>
2018-08-28 12:38:53 +0200

    testing normalization

---
+++ src/testNormRnaseq.sh
@@ -0,0 +1,2 @@
+Rscript normalisation_rnaseq.r --count_file RNA_count_BRCA_TCGA_Tum.txt
+#Rscript rnaseq_microarray_fusion.r --standardisation zscore --tables MicroArray_simulation.txt,RNAseq_simulation.txt 
